33363cbf4d3d99d20542e56a015b6927a04b6286
Sanjar Raxmatov <sanjaar.raxmatov@gmail.com>
2022-03-09 23:16:36 +0500

    fix: pages structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
-<script setup>
-	// This starter template is using Vue 3 <script setup> SFCs
-	// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-</script>
+<script setup></script>
 
 <template></template>
 
-<style>
-	#app {
-	}
-</style>
+<style></style>
